5ee52450ddd205c4e4fdf9ce967959ff5c18cd17
prakashRanjan19 <prakash.r19500@gmail.com>
2020-09-24 17:49:42 +0530

    second commit

--- app/src/main/java/com/introid/unsplashimagesearch/data/UnsplashPhoto.kt
+++ app/src/main/java/com/introid/unsplashimagesearch/data/UnsplashPhoto.kt
@@ -1,4 +1,4 @@
-package com.introid.unsplashimagesearch
+package com.introid.unsplashimagesearch.data
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
---
+++ app/src/main/java/com/introid/unsplashimagesearch/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,7 @@
+package com.introid.unsplashimagesearch.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.introid.unsplashimagesearch.R
+
+class GalleryFragment:Fragment(R.layout.fragment_gallery) {
+}
